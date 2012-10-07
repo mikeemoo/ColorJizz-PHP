71c9685e2b9946bd21ffd2b968e9404ee373eab1
Mikee Franklin <mikeefranklin@gmail.com>
2012-10-07 15:25:57 +0100

    Moved the unit tests

--- tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__.'/../lib/MischiefCollective/ColorJizz/Autoloader.php';
-MischiefCollective\ColorJizz\Autoloader::register();
-
---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__.'/../../lib/MischiefCollective/ColorJizz/Autoloader.php';
+MischiefCollective\ColorJizz\Autoloader::register();
+
